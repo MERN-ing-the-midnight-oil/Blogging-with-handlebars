b11797f45de9e6e23f891639e2015602995e62bb
Rhys Smoker <r.smoker@gmail.com>
2023-01-17 10:38:01 -0800

    formatting

--- controllers/pageRoutes.js
+++ controllers/pageRoutes.js
@@ -25,7 +25,7 @@ router.get("/", withAuth, async (req, res) => {
 		data.blogsForTemplate = blogsForTemplate; //Jamie has this, not sure why
 		// Pass serialized data and session flag into template
 		blogsForTemplate.render("home", {
-			//<-----------------//HANDLEBARS HANDLEBARS HANDLEBARS HANDLEBARS HANDLEBARS HANDLEBARS !!!
+			//^---------------------------------//HANDLEBARS  !!!
 			//renders home.handlebars
 			//blogsForTemplate, //hands the view the clean blogs
 			logged_in: req.session.logged_in, //hands the view the user logged on status
