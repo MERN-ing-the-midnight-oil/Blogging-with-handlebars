f331e75b4dbb95e678b86a289f6dd88a6cb318a1
Rhys Smoker <r.smoker@gmail.com>
2023-01-16 20:20:52 -0800

    set up express-session in server.js and package.json

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 
+//importing express-session, so we can save user data between URL reloads , for example whether the user is logged in or not
+//express-session is middleware
+const session = require("express-session");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Set up sessions
+//defaults for the sesh ID cookie is httpOnly:true, secure:false, maxAge: null. Because httpOnly is true, user won't see the sesh data even if they look in the dev tools
+const sesh = {
+	secret: "some sort of secret",
+	resave: false,
+	saveUninitialized: false,
+};
+app.use(session(sesh));
 
 const hbs = exphbs.create({});
 
