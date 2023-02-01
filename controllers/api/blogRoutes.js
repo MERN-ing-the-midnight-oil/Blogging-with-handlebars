b5f7f06a8c5dd6aa6ee08503660a9739b49da84f
Rhys Smoker <r.smoker@gmail.com>
2023-01-31 22:00:24 -0800

    fixed a user_id bug in blogRoutes so now we can create new blogs

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -3,13 +3,14 @@ const { Blogs } = require("../../models");
 const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
+	console.log(`The NEW BLOG!!`);
 	try {
-		console.log(`The NEW BLOG!!`, newBlogs);
+		console.log(`The NEW BLOG in the TRY!!`);
 		const newBlogs = await Blogs.create({
 			...req.body,
-			user_id: req.session.user.id,
+			user_id: req.session.user_id, //user_id is what the session knows the user ID as.
 		});
-
+		console.log(`The NEW BLOG variable!!`, newBlogs);
 		res.json({ ok: true });
 		//res.status(200).json(newBlogs);
 	} catch (err) {
