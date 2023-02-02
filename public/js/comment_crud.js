108c0cb7c8304637424d403090aeca5cba4bb1d0
Rhys Smoker <r.smoker@gmail.com>
2023-02-02 14:14:29 -0800

    changes

--- public/js/comment_crud.js
+++ public/js/comment_crud.js
@@ -1,8 +1,15 @@
+//this file should fetch a post to the database using commentsRoutes.js
+console.log(
+	"!!! !!! !!! this is the start of comment_crud, which does a POST of a new comment"
+);
 const newFormHandler = async (event) => {
 	event.preventDefault();
 
 	const comment = document.querySelector("#comment").value.trim();
-
+	console.log(
+		"! ! ! this is the comment that comment_crud is about to use to POST to api/comments:" +
+			comment
+	);
 	if (comment.length > 0) {
 		const response = await fetch(`/api/comments`, {
 			method: "POST",
