b45ec46b5a6d025717e1b3d32e0c1679cea3412b
Rhys Smoker <r.smoker@gmail.com>
2023-02-03 11:53:20 -0800

    fixed a bug in comments routes

--- controllers/api/commentsRoutes.js
+++ controllers/api/commentsRoutes.js
@@ -10,7 +10,8 @@ router.post("/", async (req, res) => {
 	try {
 		const newComment = await Comments.create({
 			...req.body,
-			user_id: req.session.user.id,
+			//user_id: req.session.user.id,
+			user_id: req.session.user_id,
 		});
 		console.log(
 			"! ! ! This is the newComment we just did a POST with:",
