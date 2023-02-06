36188ec403c86549584c95bc3c5dd1a6fba73060
Rhys Smoker <r.smoker@gmail.com>
2023-02-05 18:18:24 -0800

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,33 @@
 //This is my connection to the back end
 
-const Sequelize = require("sequelize");
-require("dotenv").config();
+// const Sequelize = require("sequelize");
+// require("dotenv").config();
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		host: "localhost",
-		dialect: "mysql",
-		//port: 3001,  //per advice from an ASK BCS LA abradshaw, I'm removing this line because it's interfering with 3001 on server.js
-	}
-);
+// const sequelize = new Sequelize(
+// 	process.env.DB_NAME,
+// 	process.env.DB_USER,
+// 	process.env.DB_PASSWORD,
+// 	{
+// 		host: "localhost",
+// 		dialect: "mysql",
+// 	}
+// );
 
-module.exports = sequelize;
+// module.exports = sequelize;
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PW,
+		{
+			host: "localhost",
+			dialect: "mysql",
+			port: 3306,
+		}
+	);
+}
